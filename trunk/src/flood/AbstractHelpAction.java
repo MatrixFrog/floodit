e899ad020f7a94ba97a724eb6a1d9bd91555aa93
MatrixFrog <MatrixFrog@a5f4a54e-8174-11de-a3c6-f150bf15ed86>
2009-08-08 17:45:27 +0000

    added error checking to HelpWindow and fixed indentation to use spaces instead of tabs

--- trunk/src/flood/AbstractHelpAction.java
+++ trunk/src/flood/AbstractHelpAction.java
@@ -8,29 +8,30 @@ import javax.swing.JFrame;
 import javax.swing.KeyStroke;
 
 abstract class AbstractHelpAction extends AbstractAction {
-	private JFrame window;
-	private String name;
+  private JFrame window;
+  private String name;
 
-	public AbstractHelpAction(JFrame window, String name) {
-		super(name);
-		this.window = window;
-		this.name = name;
-	}
+  public AbstractHelpAction(JFrame window, String name) {
+    super(name);
+    this.window = window;
+    this.name = name;
+  }
 
-	@Override public void actionPerformed(ActionEvent arg0) {
-		new HelpWindow(window, name + ".htm");
-	}
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+    new HelpWindow(window, name + ".htm");
+  }
 }
 
 class HelpAction extends AbstractHelpAction {
-	public HelpAction(JFrame window) {
-		super(window, "Help");
-		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
-	}
+  public HelpAction(JFrame window) {
+    super(window, "Help");
+    putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+  }
 }
 
 class AboutAction extends AbstractHelpAction {
-	public AboutAction(JFrame window) {
-		super(window, "About");
-	}
+  public AboutAction(JFrame window) {
+    super(window, "About");
+  }
 }
--- trunk/src/flood/HelpWindow.java
+++ trunk/src/flood/HelpWindow.java
@@ -6,36 +6,42 @@ import java.io.InputStream;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import org.apache.commons.io.IOUtils;
 
 class HelpWindow extends JDialog {
 
-	public static void main(String[] args) {
-		new HelpWindow(null, "help.htm");
-	}
+  public static void main(String[] args) {
+    new HelpWindow(null, "help.htm");
+  }
 
-	JLabel label;
+  JLabel label;
 
-	HelpWindow(JFrame owner, String htmlFilename) {
-		super(owner);
-		setSize(350, 250);
+  HelpWindow(JFrame owner, String helpFilename) {
+    super(owner);
+    setSize(350, 250);
 
-		label = new JLabel();
-		this.add(label);
+    label = new JLabel();
+    this.add(label);
 
-		InputStream helpStream = getClass().getResourceAsStream(htmlFilename);
+    String helpText;
+    try {
+      InputStream helpStream = getClass().getResourceAsStream(helpFilename);
+      if (helpStream == null) {
+        JOptionPane.showMessageDialog(owner, "ERROR: Could not load '"
+            + helpFilename + "'");
+        return;
+      }
+      helpText = IOUtils.toString(helpStream);
 
-		try {
-			String helpText = IOUtils.toString(helpStream);
-			label.setText(helpText);
-		} catch (IOException e) {
-			label.setText(e.toString());
-			//JOptionPane.showMessageDialog(owner, e.toString());
-		}
+    } catch (IOException e) {
+      helpText = e.toString();
+    }
+    label.setText(helpText);
 
-		this.setVisible(true);
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	}
+    this.setVisible(true);
+    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+  }
 
 }
