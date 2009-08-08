faf00b025649771ffcff7406a28f5356a85bdba4
MatrixFrog <MatrixFrog@a5f4a54e-8174-11de-a3c6-f150bf15ed86>
2009-08-08 17:45:27 +0000

    added error checking to HelpWindow and fixed indentation to use spaces instead of tabs

--- src/flood/Floodit.java
+++ src/flood/Floodit.java
@@ -31,211 +31,215 @@ import javax.swing.UIManager;
  * An attempt to clone the iPhone game Floodit
  */
 public class Floodit {
-	static final boolean DEBUG = false;
+  static final boolean DEBUG = false;
 
-	private Grid grid;
-	private int numMoves = 0;
+  private Grid grid;
+  private int numMoves = 0;
 
-	private JFrame window = new JFrame("Flood It");
-	private JPanel panel = new JPanel(new GridBagLayout());
-	private JPanel buttonPanel = new JPanel(new GridBagLayout());;
-	private JLabel numMovesLabel = new JLabel("0", JLabel.LEFT);
-	private Canvas canvas;
+  private JFrame window = new JFrame("Flood It");
+  private JPanel panel = new JPanel(new GridBagLayout());
+  private JPanel buttonPanel = new JPanel(new GridBagLayout());;
+  private JLabel numMovesLabel = new JLabel("0", JLabel.LEFT);
+  private Canvas canvas;
 
-	private List<SelectColorAction> allSelectColorActions =
-		new ArrayList<SelectColorAction>();
+  private List<SelectColorAction> allSelectColorActions = new ArrayList<SelectColorAction>();
 
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			/* Just use the ugly Swing look and feel =( */
-		}
-		new Floodit();
-	}
+  public static void main(String[] args) {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+      /* Just use the ugly Swing look and feel =( */
+    }
+    new Floodit();
+  }
 
-	public Floodit() {
-		newGame();
+  public Floodit() {
+    newGame();
 
-		addNumMovesLabel();
-		addCanvas();
-		addButtonPanel();
-		addMenuBar();
+    addNumMovesLabel();
+    addCanvas();
+    addButtonPanel();
+    addMenuBar();
 
-		window.setSize(1100, 900);
-		window.add(panel);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
+    window.setSize(1100, 900);
+    window.add(panel);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setVisible(true);
 
-		update();
-	}
+    update();
+  }
 
-	private void addNumMovesLabel() {
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.weightx = 1;
-		constraints.anchor = GridBagConstraints.NORTHEAST;
-		numMovesLabel.setFont(new Font("Dialog", Font.BOLD, 34));
-		panel.add(numMovesLabel, constraints);
-	}
+  private void addNumMovesLabel() {
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    constraints.weightx = 1;
+    constraints.anchor = GridBagConstraints.NORTHEAST;
+    numMovesLabel.setFont(new Font("Dialog", Font.BOLD, 34));
+    panel.add(numMovesLabel, constraints);
+  }
 
-	private void addCanvas() {
-		GridBagConstraints constraints = new GridBagConstraints();
-		canvas = new Canvas() {
-			@Override public void paint(Graphics g) {
-				grid.paint(g, getWidth(), getHeight());
-			}
-		};
-		canvas.setSize(750, 750);
-		constraints.gridx = 1;
-		constraints.gridy = 0;
-		constraints.weightx = 5;
-		panel.add(canvas, constraints);
-	}
+  private void addCanvas() {
+    GridBagConstraints constraints = new GridBagConstraints();
+    canvas = new Canvas() {
+      @Override
+      public void paint(Graphics g) {
+        grid.paint(g, getWidth(), getHeight());
+      }
+    };
+    canvas.setSize(750, 750);
+    constraints.gridx = 1;
+    constraints.gridy = 0;
+    constraints.weightx = 5;
+    panel.add(canvas, constraints);
+  }
 
-	private void addButtonPanel() {
-		GridBagConstraints constraints = new GridBagConstraints();
-		//constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.gridx = 1;
-		constraints.gridy = 1;
-		//constraints.gridwidth = 2;
-		panel.add(buttonPanel, constraints);
-	}
+  private void addButtonPanel() {
+    GridBagConstraints constraints = new GridBagConstraints();
+    // constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    // constraints.gridwidth = 2;
+    panel.add(buttonPanel, constraints);
+  }
 
-	private void addMenuBar() {
-		JMenuBar menuBar = new JMenuBar();
-		JMenu gameMenu = new JMenu("Game");
-		gameMenu.add(new JMenuItem(new NewGameAction(this)));
-		gameMenu.add("Undo");
-		gameMenu.add("Redo");
-		gameMenu.add("High scores");
+  private void addMenuBar() {
+    JMenuBar menuBar = new JMenuBar();
+    JMenu gameMenu = new JMenu("Game");
+    gameMenu.add(new JMenuItem(new NewGameAction(this)));
+    gameMenu.add("Undo");
+    gameMenu.add("Redo");
+    gameMenu.add("High scores");
 
-		JMenu helpMenu = new JMenu("Help");
-		helpMenu.add(new HelpAction(this.window));
-		helpMenu.add(new AboutAction(this.window));
+    JMenu helpMenu = new JMenu("Help");
+    helpMenu.add(new HelpAction(this.window));
+    helpMenu.add(new AboutAction(this.window));
 
-		menuBar.add(gameMenu);
-		menuBar.add(helpMenu);
+    menuBar.add(gameMenu);
+    menuBar.add(helpMenu);
 
-		window.setJMenuBar(menuBar);
-	}
+    window.setJMenuBar(menuBar);
+  }
 
-	private void updateButtons() {
-		buttonPanel.removeAll();
+  private void updateButtons() {
+    buttonPanel.removeAll();
 
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.weightx = 1;
-		constraints.ipadx = 20;
-		constraints.ipady = 20;
-		constraints.insets = new Insets(10, 10, 10, 10);
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    constraints.weightx = 1;
+    constraints.ipadx = 20;
+    constraints.ipady = 20;
+    constraints.insets = new Insets(10, 10, 10, 10);
 
-		for (final Color color : grid.getColors()) {
-			char name = Square.getName(color);
-			SelectColorAction action = new SelectColorAction(this, color);
-			JButton button = new JButton(action) {
-				@Override public void setEnabled(boolean enabled) {
-					super.setEnabled(enabled);
-					if (enabled) {
-						setBackground(color);
-					} else {
-						setBackground(Color.gray);
-					}
-				}
-			};
+    for (final Color color : grid.getColors()) {
+      char name = Square.getName(color);
+      SelectColorAction action = new SelectColorAction(this, color);
+      JButton button = new JButton(action) {
+        @Override
+        public void setEnabled(boolean enabled) {
+          super.setEnabled(enabled);
+          if (enabled) {
+            setBackground(color);
+          } else {
+            setBackground(Color.gray);
+          }
+        }
+      };
 
-			button.setFont(new Font("Dialog", Font.BOLD, 16));
+      button.setFont(new Font("Dialog", Font.BOLD, 16));
 
-			// Make keyboard shortcuts work
-			button.getInputMap(WHEN_IN_FOCUSED_WINDOW).put(
-					KeyStroke.getKeyStroke(name), name);
-			button.getActionMap().put(name, action);
+      // Make keyboard shortcuts work
+      button.getInputMap(WHEN_IN_FOCUSED_WINDOW).put(
+          KeyStroke.getKeyStroke(name), name);
+      button.getActionMap().put(name, action);
 
-			buttonPanel.add(button, constraints);
-			constraints.gridx++;
-		}
-	}
+      buttonPanel.add(button, constraints);
+      constraints.gridx++;
+    }
+  }
 
-	public void newGame(Dimension gridSize, int numColors) {
-		grid = new Grid(gridSize, numColors);
-		numMoves = 0;
-		allSelectColorActions.clear();
-		updateButtons();
-		//update();
-	}
+  public void newGame(Dimension gridSize, int numColors) {
+    grid = new Grid(gridSize, numColors);
+    numMoves = 0;
+    allSelectColorActions.clear();
+    updateButtons();
+    // update();
+  }
 
-	public void newGame() {
-		// TODO display a little window that lets you choose what kind of game you want
-		// For now, just hard-code a default value.
+  public void newGame() {
+    // TODO display a little window that lets you choose what kind of game you
+    // want
+    // For now, just hard-code a default value.
 
-		newGame(new Dimension(10, 10), 4);
-	}
+    newGame(new Dimension(10, 10), 4);
+  }
 
-	public void update() {
-		numMovesLabel.setText(Integer.toString(numMoves));
-		canvas.repaint();
-		panel.validate();
-		if (grid.isAllSameColor()) {
-			displayWinMessage();
-			newGame();
-			update();
-		}
-	}
+  public void update() {
+    numMovesLabel.setText(Integer.toString(numMoves));
+    canvas.repaint();
+    panel.validate();
+    if (grid.isAllSameColor()) {
+      displayWinMessage();
+      newGame();
+      update();
+    }
+  }
 
-	private void displayWinMessage() {
-		JOptionPane.showMessageDialog(window, "You win with " + numMoves + " moves!",
-				"Congratulations", JOptionPane.PLAIN_MESSAGE);
-	}
+  private void displayWinMessage() {
+    JOptionPane.showMessageDialog(window, "You win with " + numMoves
+        + " moves!", "Congratulations", JOptionPane.PLAIN_MESSAGE);
+  }
 
-	static class NewGameAction extends AbstractAction {
-		private Floodit floodit;
+  static class NewGameAction extends AbstractAction {
+    private Floodit floodit;
 
-		public NewGameAction(Floodit floodit) {
-			super("New game...");
-			this.floodit = floodit;
-	        putValue(SHORT_DESCRIPTION, "Start a new game.");
-	        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(
-	                KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-		}
+    public NewGameAction(Floodit floodit) {
+      super("New game...");
+      this.floodit = floodit;
+      putValue(SHORT_DESCRIPTION, "Start a new game.");
+      putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N,
+          ActionEvent.CTRL_MASK));
+    }
 
-		@Override public void actionPerformed(ActionEvent e) {
-			floodit.newGame();
-			floodit.update();
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      floodit.newGame();
+      floodit.update();
+    }
+  }
 
-	static class SelectColorAction extends AbstractAction {
-		private Color color;
-		private Floodit floodit;
+  static class SelectColorAction extends AbstractAction {
+    private Color color;
+    private Floodit floodit;
 
-		public SelectColorAction(Floodit floodit, Color color) {
-			super(Square.colorsNames().get(color).toString().toUpperCase());
-			this.floodit = floodit;
-			this.color = color;
-			//putValue(ACTION_COMMAND_KEY, Square.colorsNames().get(color).toString());
-			putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(
-					Square.colorsNames().get(color), KeyEvent.CTRL_MASK));
-			floodit.allSelectColorActions.add(this);
-			if (color.equals(floodit.grid.getUpperLeftColor())) {
-				setEnabled(false);
-			}
-		}
+    public SelectColorAction(Floodit floodit, Color color) {
+      super(Square.colorsNames().get(color).toString().toUpperCase());
+      this.floodit = floodit;
+      this.color = color;
+      // putValue(ACTION_COMMAND_KEY,
+      // Square.colorsNames().get(color).toString());
+      putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(Square.colorsNames()
+          .get(color), KeyEvent.CTRL_MASK));
+      floodit.allSelectColorActions.add(this);
+      if (color.equals(floodit.grid.getUpperLeftColor())) {
+        setEnabled(false);
+      }
+    }
 
-		@Override public void actionPerformed(ActionEvent e) {
-			for (SelectColorAction action : floodit.allSelectColorActions) {
-				action.setEnabled(true);
-			}
-			this.setEnabled(false);
-			floodit.grid.changeUpperLeftGroupToColor(color);
-			floodit.numMoves++;
-			floodit.update();
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      for (SelectColorAction action : floodit.allSelectColorActions) {
+        action.setEnabled(true);
+      }
+      this.setEnabled(false);
+      floodit.grid.changeUpperLeftGroupToColor(color);
+      floodit.numMoves++;
+      floodit.update();
+    }
+  }
 
-	static abstract class ShowHelpAction extends AbstractAction {
+  static abstract class ShowHelpAction extends AbstractAction {
 
-
-	}
+  }
 }
--- src/flood/Grid.java
+++ src/flood/Grid.java
@@ -13,176 +13,177 @@ import java.util.List;
 
 import common.RandomUtils;
 import common.swingutils.Points;
+
 public class Grid {
 
-	private Square[][] data;
-	private Collection<Square> upperLeftGroup = new HashSet<Square>();
-	private List<Color> colors = new ArrayList<Color>();
+  private Square[][] data;
+  private Collection<Square> upperLeftGroup = new HashSet<Square>();
+  private List<Color> colors = new ArrayList<Color>();
 
-	public Grid(int width, int height, int numColors) {
-		initColors(numColors);
-		data = new Square[width][height];
-		for (int i=0; i<width; i++) {
-			for (int j=0; j<height; j++) {
-				data[i][j] = new Square(RandomUtils.choice(colors));
-			}
-		}
-		upperLeftGroup.add(get(0,0));
-		update();
-	}
+  public Grid(int width, int height, int numColors) {
+    initColors(numColors);
+    data = new Square[width][height];
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        data[i][j] = new Square(RandomUtils.choice(colors));
+      }
+    }
+    upperLeftGroup.add(get(0, 0));
+    update();
+  }
 
-	public Grid(Dimension gridSize, int numColors) {
-		this(gridSize.width, gridSize.height, numColors);
-	}
+  public Grid(Dimension gridSize, int numColors) {
+    this(gridSize.width, gridSize.height, numColors);
+  }
 
-	private void initColors(int numColors) {
-		List<Color> allColors = Square.colors();
-		Collections.shuffle(allColors);
+  private void initColors(int numColors) {
+    List<Color> allColors = Square.colors();
+    Collections.shuffle(allColors);
 
-		for (int i=0; i<numColors; i++) {
-			colors.add(allColors.get(i));
-		}
-	}
+    for (int i = 0; i < numColors; i++) {
+      colors.add(allColors.get(i));
+    }
+  }
 
-	public Color getUpperLeftColor() {
-		return get(0,0).getColor();
-	}
+  public Color getUpperLeftColor() {
+    return get(0, 0).getColor();
+  }
 
-	public void changeUpperLeftGroupToColor(Color color) {
-		for (Square square : upperLeftGroup) {
-			square.setColor(color);
-		}
-		update();
-	}
+  public void changeUpperLeftGroupToColor(Color color) {
+    for (Square square : upperLeftGroup) {
+      square.setColor(color);
+    }
+    update();
+  }
 
-	private void expandUpperLeftGroup() {
-		boolean squareWasAdded;
-		do {
-			squareWasAdded=false;
-			for (int x=0; x<getWidth(); x++) {
-				for (int y=0; y<getHeight(); y++) {
-					Square square = get(x, y);
-					for (Square neighbor : getNeighbors(x, y)) {
-						if (upperLeftGroup.contains(square) &&
-								square.sameColor(neighbor) &&
-								!upperLeftGroup.contains(neighbor)) {
+  private void expandUpperLeftGroup() {
+    boolean squareWasAdded;
+    do {
+      squareWasAdded = false;
+      for (int x = 0; x < getWidth(); x++) {
+        for (int y = 0; y < getHeight(); y++) {
+          Square square = get(x, y);
+          for (Square neighbor : getNeighbors(x, y)) {
+            if (upperLeftGroup.contains(square) && square.sameColor(neighbor)
+                && !upperLeftGroup.contains(neighbor)) {
 
-							upperLeftGroup.add(neighbor);
-							squareWasAdded = true;
+              upperLeftGroup.add(neighbor);
+              squareWasAdded = true;
 
-						}
-					}
-				}
-			}
-		} while (squareWasAdded);
-	}
+            }
+          }
+        }
+      }
+    } while (squareWasAdded);
+  }
 
-	/**
-	 * @return All the squares orthogonally adjacent to the square at (i, j).
-	 */
-	List<Square> getNeighbors(int i, int j) {
-		List<Square> neighbors = new ArrayList<Square>();
-		for (Point p : Points.getOrthoNeighbors(new Point(i, j))) {
-			if (this.contains(p)) {
-				neighbors.add(this.get(p));
-			}
-		}
-		return neighbors;
-	}
+  /**
+   * @return All the squares orthogonally adjacent to the square at (i, j).
+   */
+  List<Square> getNeighbors(int i, int j) {
+    List<Square> neighbors = new ArrayList<Square>();
+    for (Point p : Points.getOrthoNeighbors(new Point(i, j))) {
+      if (this.contains(p)) {
+        neighbors.add(this.get(p));
+      }
+    }
+    return neighbors;
+  }
 
-	private boolean contains(Point p) {
-		return (0<=p.x && p.x<getWidth() &&
-				0<=p.y && p.y<getHeight());
-	}
+  private boolean contains(Point p) {
+    return (0 <= p.x && p.x < getWidth() && 0 <= p.y && p.y < getHeight());
+  }
 
-	public void paint(Graphics g, int width, int height) {
-		int squareWidth = width / getWidth();
-		int squareHeight = height / getHeight();
-		for (int x=0; x<getWidth(); x++) {
-			for (int y=0; y<getHeight(); y++) {
-				this.get(x,y).paint(g,
-						x*squareWidth, y*squareHeight,
-						squareWidth, squareHeight, upperLeftGroup.contains(get(x,y)));
-			}
-		}
-	}
+  public void paint(Graphics g, int width, int height) {
+    int squareWidth = width / getWidth();
+    int squareHeight = height / getHeight();
+    for (int x = 0; x < getWidth(); x++) {
+      for (int y = 0; y < getHeight(); y++) {
+        this.get(x, y).paint(g, x * squareWidth, y * squareHeight, squareWidth,
+            squareHeight, upperLeftGroup.contains(get(x, y)));
+      }
+    }
+  }
 
-	public void update() {
-		expandUpperLeftGroup();
-	}
+  public void update() {
+    expandUpperLeftGroup();
+  }
 
-	public boolean isAllSameColor() {
-		Square upperLeft = get(0,0);
-		for (Square square : allSquares()) {
-			if (!upperLeft.sameColor(square)) {
-				return false;
-			}
-		}
-		return true;
-	}
+  public boolean isAllSameColor() {
+    Square upperLeft = get(0, 0);
+    for (Square square : allSquares()) {
+      if (!upperLeft.sameColor(square)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	public Iterable<Square> allSquares() {
-		return new Iterable<Square>() {
-			public Iterator<Square> iterator() {
-				return new Iterator<Square>() {
-					private int x=0, y=0;
+  public Iterable<Square> allSquares() {
+    return new Iterable<Square>() {
+      public Iterator<Square> iterator() {
+        return new Iterator<Square>() {
+          private int x = 0, y = 0;
 
-					@Override public boolean hasNext() {
-						return y != getHeight();
-					}
+          @Override
+          public boolean hasNext() {
+            return y != getHeight();
+          }
 
-					@Override public Square next() {
-						Square square = get(x,y);
-						x++;
-						if (x == getWidth()) {
-							x = 0;
-							y++;
-						}
-						return square;
-					}
+          @Override
+          public Square next() {
+            Square square = get(x, y);
+            x++;
+            if (x == getWidth()) {
+              x = 0;
+              y++;
+            }
+            return square;
+          }
 
-					@Override public void remove() {
-						throw new UnsupportedOperationException();
-					}
-				};
-			}
-		};
-	}
+          @Override
+          public void remove() {
+            throw new UnsupportedOperationException();
+          }
+        };
+      }
+    };
+  }
 
-	public int getWidth() {
-		return data.length;
-	}
+  public int getWidth() {
+    return data.length;
+  }
 
-	public int getHeight() {
-		return data[0].length;
-	}
+  public int getHeight() {
+    return data[0].length;
+  }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		for (int i=0; i<getWidth(); i++) {
-			for (int j=0; j<getHeight(); j++) {
-				Square square = get(i,j);
-				String str = square.toString();
-				if (upperLeftGroup.contains(square)) {
-					str = str.toUpperCase();
-				}
-				sb.append(str);
-			}
-			sb.append('\n');
-		}
-		return sb.toString();
-	}
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < getWidth(); i++) {
+      for (int j = 0; j < getHeight(); j++) {
+        Square square = get(i, j);
+        String str = square.toString();
+        if (upperLeftGroup.contains(square)) {
+          str = str.toUpperCase();
+        }
+        sb.append(str);
+      }
+      sb.append('\n');
+    }
+    return sb.toString();
+  }
 
-	private Square get(Point p) {
-		return get(p.x, p.y);
-	}
+  private Square get(Point p) {
+    return get(p.x, p.y);
+  }
 
-	public Square get(int i, int j) {
-		return data[i][j];
-	}
+  public Square get(int i, int j) {
+    return data[i][j];
+  }
 
-	public List<Color> getColors() {
-		return colors;
-	}
+  public List<Color> getColors() {
+    return colors;
+  }
 }
