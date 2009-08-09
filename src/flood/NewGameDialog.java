4dd2b8994d3d903608254238520e6d11dd321d17
MatrixFrog <MatrixFrog@a5f4a54e-8174-11de-a3c6-f150bf15ed86>
2009-08-09 00:37:13 +0000

    further awesomeness

--- src/flood/NewGameDialog.java
+++ src/flood/NewGameDialog.java
@@ -6,35 +6,34 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
-import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 
 public class NewGameDialog extends JDialog {
 
   private JPanel panel = new JPanel(new GridBagLayout());
-  //private JTextField width = new JTextField(10);
-  //private JTextField height = new JTextField(10);
-  //private JSpinner numColors = new JSpinner();
+  GridBagConstraints constraints = new GridBagConstraints();
 
   public NewGameDialog(Floodit owner) {
     super(owner, "New Game", true);
     this.add(panel);
 
-    GridBagConstraints constraints = new GridBagConstraints();
-    constraints.insets = new Insets(20, 10, 20, 10);
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.ipadx = 30;
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    constraints.anchor = GridBagConstraints.CENTER;
+    initConstraints();
 
     for (JLabel label : headers()) {
       panel.add(label, constraints);
@@ -42,33 +41,56 @@ public class NewGameDialog extends JDialog {
     }
 
     constraints.gridy++;
-    constraints.gridx=0;
     for (String gameType : new String[] {"Beginner", "Intermediate", "Advanced"}) {
-      addRow(gameType, constraints, false);
+      constraints.gridx=0;
+      addRow(gameType, false);
       constraints.gridy++;
     }
-    addRow("Custom", constraints, true);
+    constraints.gridx=0;
+    addRow("Custom", true);
+    constraints.gridy++;
+
+    constraints.gridx = 0;
+    constraints.gridwidth = GridBagConstraints.REMAINDER;
+    constraints.anchor = GridBagConstraints.CENTER;
+    constraints.fill = GridBagConstraints.NONE;
+    panel.add(new JButton(new AbstractAction() {
+      {
+        putValue(NAME, "Cancel");
+      }
+      @Override public void actionPerformed(ActionEvent arg0) {
+        // TODO (maybe) have this do something other than close the entire game
+        ((Floodit) NewGameDialog.this.getOwner()).close();
+      }
+    }), constraints);
+
 
     this.setSize(this.getPreferredSize());
     this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     this.setVisible(true);
   }
 
+  private void initConstraints() {
+    constraints.insets = new Insets(20, 10, 20, 10);
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.ipadx = 30;
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    constraints.anchor = GridBagConstraints.CENTER;
+  }
+
   /**
-   * Warning: This method will change the gridx attribute of the GridBigConstraints object
-   * that is passed in.
-   *
    * @param gameTypeName The type of game. Should be in GameSettings.gameTypes.keySet()
    * @param constraints The constraints that will be used to add the row (with the exception
    * of .gridx which obviously will vary
    */
-  private void addRow(String gameTypeName, GridBagConstraints constraints, boolean enable) {
+  private void addRow(String gameTypeName, boolean enable) {
     final GameSettings settings = GameSettings.get(gameTypeName);
 
     constraints.gridx = 0;
     JButton button = new JButton(gameTypeName);
     button.addActionListener(new ActionListener() {
-      @Override public void actionPerformed(ActionEvent arg0) {
+      @Override public void actionPerformed(ActionEvent e) {
         ((Floodit) getOwner()).newGame(settings);
         ((Floodit) getOwner()).update();
         NewGameDialog.this.dispose();
@@ -77,18 +99,68 @@ public class NewGameDialog extends JDialog {
     panel.add(button, constraints);
 
     constraints.gridx++;
-    JTextField widthField = new JTextField(Integer.toString(settings.width));
+    JFormattedTextField widthField =
+      new JFormattedTextField(NumberFormat.getNumberInstance());
+    widthField.setText(Integer.toString(settings.width));
+    widthField.getDocument().addDocumentListener(new DocumentListener() {
+      @Override public void changedUpdate(DocumentEvent e) {
+        throw new RuntimeException();
+      }
+      @Override public void insertUpdate(DocumentEvent e) {
+        handleChange(e);
+      }
+      @Override public void removeUpdate(DocumentEvent e) {
+        handleChange(e);
+      }
+      private void handleChange(DocumentEvent e) {
+        try {
+          settings.width = Integer.parseInt(
+              e.getDocument().getText(0, e.getDocument().getLength()));
+        } catch (NumberFormatException f) {
+          settings.width = 0;
+        } catch (BadLocationException f) {
+          throw new RuntimeException(f);
+        }
+      }
+    });
     widthField.setEnabled(enable);
     panel.add(widthField, constraints);
 
     constraints.gridx++;
-    JTextField heightField = new JTextField(Integer.toString(settings.height));
+    JFormattedTextField heightField =
+      new JFormattedTextField(Integer.toString(settings.height));
+    heightField.getDocument().addDocumentListener(new DocumentListener() {
+      @Override public void changedUpdate(DocumentEvent e) {
+        throw new RuntimeException();
+      }
+      @Override public void insertUpdate(DocumentEvent e) {
+        handleChange(e);
+      }
+      @Override public void removeUpdate(DocumentEvent e) {
+        handleChange(e);
+      }
+      private void handleChange(DocumentEvent e) {
+        try {
+          settings.height = Integer.parseInt(
+              e.getDocument().getText(0, e.getDocument().getLength()));
+        } catch (NumberFormatException f) {
+          settings.height = 0;
+        } catch (BadLocationException f) {
+          throw new RuntimeException(f);
+        }
+      }
+    });
     heightField.setEnabled(enable);
     panel.add(heightField, constraints);
 
     constraints.gridx++;
     JSpinner numColors = new JSpinner(
         new SpinnerNumberModel(settings.numColors, 2, Square.colors().size(), 1));
+    numColors.addChangeListener(new ChangeListener() {
+      @Override public void stateChanged(ChangeEvent e) {
+        settings.numColors = (Integer) ((JSpinner) e.getSource()).getValue();
+      }
+    });
     numColors.setEnabled(enable);
     panel.add(numColors, constraints);
   }
@@ -105,5 +177,4 @@ public class NewGameDialog extends JDialog {
     }
     return headers;
   }
-
 }
