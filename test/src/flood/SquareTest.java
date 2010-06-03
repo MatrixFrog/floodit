8347a6666e60d1c56e27db6be03c7105104c646d
MatrixFrog <MatrixFrog@a5f4a54e-8174-11de-a3c6-f150bf15ed86>
2010-06-03 03:17:36 +0000

    updating unit tests to use Theories. so fancy.

--- test/src/flood/SquareTest.java
+++ test/src/flood/SquareTest.java
@@ -5,13 +5,20 @@ import static org.junit.Assert.assertTrue;
 
 import java.awt.Color;
 
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theory;
 
 
 public class SquareTest {
 
-  @Test public void testClone() {
-    Square orig = new Square(Color.red);
+  @DataPoints
+  public static final Square[] squares = {
+    new Square(Color.RED),
+    new Square(Color.BLACK),
+    new Square(Color.GREEN)
+  };
+
+  @Theory public void testClone(Square orig) {
     Square clone = orig.clone();
     assertNotSame(orig, clone);
     assertTrue(orig.sameColor(clone));
